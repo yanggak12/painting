befd02db8fd0b7050135c6965a916feb5c3bac2b
양윤상 <yanggak12@naver.com>
2021-09-07 22:52:05 +0900

    #2.1. 2D Context

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#000000";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function stopPainting() {
   painting = false;
 }
+
+function startPainting() {
+  painting = true;
+}
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  //console.log(x, y);
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
